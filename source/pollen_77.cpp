536543eab0caaa995cef8c6b965499b298dbaa9f
Danielle Navarro <djnavarro@protonmail.com>
2023-07-19 22:16:52 +1000

    reset repo

---
+++ source/pollen_77.cpp
@@ -0,0 +1,165 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// function to be called from R
+// [[Rcpp::export]]
+NumericMatrix raster_data(int iter, int layers, int pixels, double zoom, double alpha) {
+  
+  NumericMatrix image(pixels, pixels); // initially zero
+  NumericMatrix coeffs(9, layers);
+  
+  // set coefficients
+  for(int i = 0; i < 9; i++) {
+    for(int j = 0; j < layers; j++) {
+      coeffs(i, j) = R::runif(-2, 2);
+    }
+  }
+  
+  // image level constant: these affect the celestial spheres
+  double y_offset = R::runif(-.5, .5);
+  double x_offset = R::runif(-.5, .5);
+  double r_offset = R::runif(0, 6.28);
+  
+  // image level constant: these shape the lissajous attractor
+  // scale multiplies the bigger one, s_offset rotates it
+  double s_offset = R::runif(0, 6.28);
+  double scale = R::runif(2, 6);
+  
+  // set image matrix to zeros
+  for(int r = 0; r < pixels; r++) {
+    for(int c = 0; c < pixels; c++) {
+      image(c, r) = 0;
+    }
+  }
+  
+  // iterate
+  int layer;
+  int variant;
+  double petal;
+  
+  // convenience variables
+  double s = 0;
+  double u = .3;
+  
+  // offsets
+  double x_sh = 0;
+  double y_sh = 0;
+  
+  // indices for storing coordinates
+  int x_ind;
+  int y_ind;
+  
+  // values for current state
+  double x = 0;
+  double y = 0;
+  double z = 0;
+  
+  // values for storing temp coords
+  double x_tmp = 0;
+  double y_tmp = 0;
+  
+  // values for previous state
+  double x_old = R::runif(-1, 1);
+  double y_old = R::runif(-1, 1);
+  double z_old = R::runif(-1, 1);
+
+  // rotation
+  int n_sym = 1 + rand() % 5;
+  double rr = 0;
+  
+  
+  // introduces some noise and echo to the attractor 
+  const double v_shift = 10;
+  
+  // iterate...
+  for(int it = 1; it < iter; it++) {
+    
+    layer = rand() % layers;   // which affine transform to use?
+    variant = rand() % 5;      // which variant function to use?
+    
+    // coordinates after random transform
+    x = x_old + coeffs(0, layer) * x_old + coeffs(1, layer) * y_old + coeffs(2, layer);
+    y = y_old + coeffs(3, layer) * x_old + coeffs(4, layer) * y_old + coeffs(5, layer);
+    z = z_old + coeffs(6, layer) * x_old + coeffs(7, layer) * y_old + coeffs(8, layer);
+    
+    // apply function to the transformed coordinates
+    
+    // variant 0 is the path itself
+    
+    if(variant == 0) {
+      s = x*x + y*y + z*z + 2;
+      x = x + v_shift * x/s;
+      y = y + v_shift * y/s;
+      z = z + v_shift * z/s;
+      if(z < 0) z = 0;
+      
+    // variants 1 and 2 are the celestial spheres 
+    } else if(variant == 1){
+      s = cos(x * y);
+      x = cos(x) + x_offset;
+      y = cos(y) - 1.2 + y_offset;
+      u = x*x + y*y;
+      x_tmp = s * x / u;
+      y_tmp = s * y / u;
+      x = x_tmp * cos(r_offset) - y_tmp * sin(r_offset);
+      y = x_tmp * sin(r_offset) + y_tmp * cos(r_offset);
+      z = 1 + sin(z);
+    } else if(variant == 2){
+      s = cos(x * y);
+      x = cos(x) + x_offset;
+      y = cos(y) - .9 + y_offset;
+      u = x*x + y*y;
+      x_tmp = scale * s * x / u;
+      y_tmp = scale * s * y / u;
+      x = x_tmp * cos(r_offset) - y_tmp * sin(r_offset);
+      y = x_tmp * sin(r_offset) + y_tmp * cos(r_offset);
+      z = 1 + sin(z);
+    
+    // variants 3 and 4 are the lissajous attractor 
+    } else if(variant == 3){
+      s = x * y;
+      x_tmp = cos(s) * sin(s);
+      y_tmp = sin(s);
+      x = x_tmp * cos(s_offset) - y_tmp * sin(s_offset);
+      y = x_tmp * sin(s_offset) + y_tmp * cos(s_offset);
+      z = 1 + sin(z);
+    } else {
+      s = x * y + s_offset;
+      x_tmp = cos(s) * scale * sin(s);
+      y_tmp = sin(s) * scale;
+      x = x_tmp * cos(s_offset) - y_tmp * sin(s_offset);
+      y = x_tmp * sin(s_offset) + y_tmp * cos(s_offset);
+      z = 1 + sin(z);
+    }
+    
+    // rotate
+    rr = r_offset + 6.28 * (rand() % (n_sym + 2)) / (n_sym + 1);
+    x = x_tmp * cos(rr) - y_tmp * sin(rr);
+    y = x_tmp * sin(rr) + y_tmp * cos(rr);
+    z = z/2 + 3 * rr/2; 
+    
+    
+    
+    // compute indices to be updated
+    x_ind = int (x * pixels * zoom) + pixels/2;
+    y_ind = int (y * pixels * zoom) + pixels/2;
+    
+    // store results if they fall within the range
+    //if(variant == 0) {
+      if(x_ind >= 0 & x_ind < pixels) {
+        if(y_ind >= 0 & y_ind < pixels) {
+          image(x_ind, y_ind) = alpha * z + (1- alpha) * image(x_ind, y_ind);
+        }
+      }
+    //}
+    
+    // move new to old
+    x_old = x;
+    y_old = y;
+    z_old = z * .5 + z_old * .5; 
+  }
+  
+  return image;
+}
+
+
